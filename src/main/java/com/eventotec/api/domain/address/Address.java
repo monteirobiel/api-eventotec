05221c0f41f748cd8f7856ed82fc27f331f23ba5
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-04 17:56:29 -0300

    feat: add address entity with annotations and attributes.

---
+++ src/main/java/com/eventotec/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventotec.api.domain.address;
+
+import com.eventotec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String uf;
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
